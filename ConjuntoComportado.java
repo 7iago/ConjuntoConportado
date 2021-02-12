aa062f47208287d20d658b1bf65e593c57694e70
Tiago <74260711+7iago@users.noreply.github.com>
2021-02-12 10:24:22 -0300

    mod 1

--- ConjuntoComportado.java
+++ ConjuntoComportado.java
@@ -6,7 +6,10 @@ import java.util.Set;
 public class ConjuntoComportado {
 
 	public static void main(String[] args) {
-		Set<String> listaAprovados = new HashSet<>;
+		Set<String> listaAprovados = new HashSet<>();
+		listaAprovados.add("Ana");
+		listaAprovados.add("Carlos");
+		System.out.println(listaAprovados);
 
 
 	}
